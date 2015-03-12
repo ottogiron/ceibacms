c6c1717940e419742d117886113bcaf2a39ed7f7
Otto Giron <ottog2486@gmail.com>
2015-03-11 23:19:05 -0600

    Removed useless error handler

--- modules/core/ccm/web/template-engines/ceiba.js
+++ modules/core/ccm/web/template-engines/ceiba.js
@@ -6,9 +6,6 @@ module.exports = function(options){
   store = options.store;
 
   return function(filePath, options, cb){
-      if(filePath === '/ceibacms/modules/user/error.html'){
-        return cb(null, 'error');
-      }
       var pageNode = store.getNode(options.pageNodePath);
       var engine = pageNode.content.engine;
       var templateEngine = templateEngines[engine];
