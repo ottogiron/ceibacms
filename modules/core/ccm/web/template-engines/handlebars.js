0ed659401393d10391b74c7404ae5db7df7fd87c
Otto Giron <ottog2486@gmail.com>
2015-03-15 16:42:16 -0600

    Fixed undefined function in handlebars template engine

--- modules/core/ccm/web/template-engines/handlebars.js
+++ modules/core/ccm/web/template-engines/handlebars.js
@@ -4,10 +4,10 @@ var Handlebars = require('handlebars');
 
 module.exports = function(pageNode, cb) {
   templateManager.loadTemplate(pageNode, function(err, htmlSource){
-    if(err){return next(err);}
+    if(err){return cb(err);}
     var context = {
       page: pageNode
-    };    
+    };
     var template = Handlebars.compile(htmlSource);
     var html = template(context);
     cb(null, html);
