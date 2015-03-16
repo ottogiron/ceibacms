b8ae692c3f9f4409b0a728bd616af068315fef3b
Otto Giron <ottog2486@gmail.com>
2015-03-15 18:29:42 -0600

    Configured new route for bower components

--- modules/core/ccm/web/routes/static.js
+++ modules/core/ccm/web/routes/static.js
@@ -4,9 +4,10 @@ var path = require('path');
 
 var templateEngines;
 var store;
+var projectRoot =  '../../../../../';
 /* GET users listing. */
 router.get(/\/(.*)(styles|scripts)\/.*\.(js|css)/, function(req, res, next) {
-    var rootPath = path.join(__dirname, '../../../../../','modules/user')
+    var rootPath = path.join(__dirname, projectRoot ,'modules/user')
     var options = {
       root: rootPath
     };
@@ -21,6 +22,22 @@ router.get(/\/(.*)(styles|scripts)\/.*\.(js|css)/, function(req, res, next) {
     });
 });
 
+router.get(/\/bower_components\/(.*\.(css|html|js))/, function(req, res, next){
+  var rootPath = path.join(__dirname, projectRoot, 'bower_components')
+  var options = {
+    root: rootPath
+  };
+  var filePath = req.params[0]; //first regular expression match
+
+
+  res.sendFile(filePath, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+  });
+
+});
 
 
 module.exports = function(){
