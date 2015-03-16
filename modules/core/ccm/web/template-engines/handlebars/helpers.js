045594d9a456061fc1781c599fa4f4570d03cb32
Otto Giron <ottog2486@gmail.com>
2015-03-15 21:32:00 -0600

    Web components dependencies ready

--- modules/core/ccm/web/template-engines/handlebars/helpers.js
+++ modules/core/ccm/web/template-engines/handlebars/helpers.js
@@ -1,17 +1,23 @@
 var Handlebars = require('handlebars');
 
+
+
 var includeComponentHelper = function(options){
    var path = options.hash.path;
    var ctype = options.hash.ctype;
-   var data = options.root;
+   var data = options.data.root;
+   //TODO: Utilitary module for getting meta info of component. This might be fixed implementing an actual component class
    var pathTokens = ctype.split('/');
    var moduleName = pathTokens[3];
    var componentName = pathTokens[pathTokens.length -1];
    var webComponentName = moduleName + '-' + componentName;
+   data.dependencies.push(ctype);
    return new Handlebars.SafeString(
      "<" + webComponentName + "></" + webComponentName + ">"
    );
 };
+
+
 
 
 module.exports = function(){
--- modules/core/ccm/web/template-engines/handlebars/index.js
+++ modules/core/ccm/web/template-engines/handlebars/index.js
@@ -2,12 +2,36 @@ var templateManager = require('../../../api/templating/template-manager');
 
 var Handlebars = require('handlebars');
 var helpers = require('./helpers')();
+var jsdom  = require('jsdom');
+
+
 
 function registerHelpers(){
   Object.keys(helpers).forEach(function(key){
     var helperDefinition = helpers[key];
     Handlebars.registerHelper(key, helperDefinition);
   });
+}
+
+function wireDependencies(html, dependencies){
+
+  var window = jsdom.jsdom(html).defaultView;
+  var $ = require('jquery')(window);
+
+  var $html = $('html');
+
+  if(!$html.find('head').length){
+     $html.prepend('<head></head>');
+  }
+  var $head = $html.find('head');
+
+  //TODO: This should depende on the component type webcompoent, angular, etc
+  dependencies.forEach(function(dependencyPath){
+    $head.prepend('<link rel="import" href="'+ dependencyPath +'" >');
+  });
+
+  return '<html>'+ $html.html() + '<html>';
+
 }
 
 module.exports = function(pageNode, store, cb) {
@@ -23,6 +47,7 @@ module.exports = function(pageNode, store, cb) {
     };
     var template = Handlebars.compile(htmlSource);
     var html = template(context);
-    cb(null, html);
+    var wiredHTML = wireDependencies(html, dependencies);
+    cb(null, wiredHTML);
   });
 };
